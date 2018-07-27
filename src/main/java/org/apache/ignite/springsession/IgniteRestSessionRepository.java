449e5d43ded38636abaae697f6ec89ce7fb7f5ab
antkr <antkr.dev@gmail.com>
2018-07-27 21:37:41 +0300

    Fixed serialization/deserialization of Map of custom session attributes

--- src/main/java/org/apache/ignite/springsession/IgniteRestSessionRepository.java
+++ src/main/java/org/apache/ignite/springsession/IgniteRestSessionRepository.java
@@ -4,18 +4,25 @@ import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
-import java.net.URLEncoder;
 import java.util.HashMap;
 import java.util.Map;
 import javax.annotation.PostConstruct;
+import org.apache.commons.codec.DecoderException;
+import org.apache.commons.codec.binary.Hex;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
+import org.springframework.core.convert.ConversionService;
+import org.springframework.core.convert.TypeDescriptor;
+import org.springframework.core.convert.support.GenericConversionService;
+import org.springframework.core.serializer.support.DeserializingConverter;
+import org.springframework.core.serializer.support.SerializingConverter;
 import org.springframework.session.SessionRepository;
 import org.springframework.stereotype.Component;
 
@@ -58,6 +65,10 @@ public class IgniteRestSessionRepository implements SessionRepository<IgniteSess
 
     private Integer defaultMaxInactiveInterval;
 
+    private ObjectMapper mapper;
+
+    private ConversionService conversionService;
+
     public IgniteRestSessionRepository() {
 
     }
@@ -75,8 +86,38 @@ public class IgniteRestSessionRepository implements SessionRepository<IgniteSess
         this.port = port;
     }
 
+    private static GenericConversionService createDefaultConversionService() {
+        GenericConversionService converter = new GenericConversionService();
+        converter.addConverter(IgniteSession.class, byte[].class,
+            new SerializingConverter());
+        converter.addConverter(byte[].class, IgniteSession.class,
+            new DeserializingConverter());
+        return converter;
+}
+
+    private String serialize(IgniteSession attributeValue) {
+        byte[] value = (byte[]) this.conversionService.convert(attributeValue,
+            TypeDescriptor.valueOf(IgniteSession.class),
+            TypeDescriptor.valueOf(byte[].class));
+        return Hex.encodeHexString(value);
+    }
+
+    private IgniteSession deserialize(String value) throws DecoderException {
+        if ("null".equals(value))
+            return null;
+        return (IgniteSession) this.conversionService.convert(Hex.decodeHex(value.toCharArray()),
+            TypeDescriptor.valueOf(byte[].class),
+            TypeDescriptor.valueOf(IgniteSession.class));
+    }
+
     @PostConstruct
     public void init() {
+        this.mapper = new ObjectMapper();
+        this.mapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.NONE);
+        this.mapper.setVisibility(PropertyAccessor.FIELD, JsonAutoDetect.Visibility.ANY);
+
+        this.conversionService = createDefaultConversionService();
+
         CloseableHttpClient client = getHttpClient(this.ip, this.port);
 
         HttpResponse res;
@@ -135,18 +176,13 @@ public class IgniteRestSessionRepository implements SessionRepository<IgniteSess
     @Override public IgniteSession createSession() {
         IgniteSession session = new IgniteSession();
 
-        if (this.defaultMaxInactiveInterval != null) {
+        if (this.defaultMaxInactiveInterval != null)
             session.setMaxInactiveIntervalInSeconds(this.defaultMaxInactiveInterval);
-        }
 
         return session;
     }
 
     @Override public void save(IgniteSession session) {
-        ObjectMapper mapper = new ObjectMapper();
-        mapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.NONE);
-        mapper.setVisibility(PropertyAccessor.FIELD, JsonAutoDetect.Visibility.ANY);
-
         CloseableHttpClient client = getHttpClient(this.ip, this.port);
 
         HttpResponse res;
@@ -156,7 +192,8 @@ public class IgniteRestSessionRepository implements SessionRepository<IgniteSess
             ss.put(CMD, PUT);
             ss.put(CACHE_NAME, sessionCacheName);
             ss.put(KEY, session.getId());
-            ss.put(VALUE, URLEncoder.encode(mapper.writeValueAsString(session), "UTF-8"));
+            ss.put(VALUE, serialize(session));
+
             res = client.execute(buildCacheRequest(this.ip, this.port, ss));
 
             try {
@@ -200,11 +237,9 @@ public class IgniteRestSessionRepository implements SessionRepository<IgniteSess
 
                 try {
                     is = res.getEntity().getContent();
-                    ObjectMapper mapper = new ObjectMapper();
-                    mapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.NONE);
-                    mapper.setVisibility(PropertyAccessor.FIELD, JsonAutoDetect.Visibility.ANY);
+
                     JsonNode node = mapper.readTree(is).get("response");
-                    session = mapper.readValue(node.asText(), IgniteSession.class);
+                    session = deserialize(node.asText());
                 }
                 finally {
                     if (is != null )
@@ -219,9 +254,9 @@ public class IgniteRestSessionRepository implements SessionRepository<IgniteSess
             e.printStackTrace();
         }
 
-        if (session == null) {
+        if (session == null)
             return null;
-        }
+
         if (session.isExpired()) {
             delete(id);
             return null;
@@ -231,10 +266,6 @@ public class IgniteRestSessionRepository implements SessionRepository<IgniteSess
     }
 
     @Override public void delete(String id) {
-        ObjectMapper mapper = new ObjectMapper();
-        mapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.NONE);
-        mapper.setVisibility(PropertyAccessor.FIELD, JsonAutoDetect.Visibility.ANY);
-
         CloseableHttpClient client = getHttpClient(this.ip, this.port);
 
         HttpResponse res;
@@ -268,5 +299,14 @@ public class IgniteRestSessionRepository implements SessionRepository<IgniteSess
 
     public void setDefaultMaxInactiveInterval(Integer dfltMaxInactiveInterval) {
         defaultMaxInactiveInterval = dfltMaxInactiveInterval;
+    }
+
+    private String readStream(InputStream is) throws IOException {
+        ByteArrayOutputStream result = new ByteArrayOutputStream();
+        byte[] buffer = new byte[1024];
+        int length;
+        while ((length = is.read(buffer)) != -1)
+            result.write(buffer, 0, length);
+        return result.toString("UTF-8");
     }
 }
--- src/test/java/org/apache/ignite/springsession/IgniteRestRepositoryIntegrationTests.java
+++ src/test/java/org/apache/ignite/springsession/IgniteRestRepositoryIntegrationTests.java
@@ -3,6 +3,7 @@ package org.apache.ignite.springsession;
 import java.util.concurrent.TimeUnit;
 import org.apache.ignite.springsession.config.annotation.web.http.EnableRestIgniteHttpSession;
 import org.apache.ignite.springsession.config.annotation.web.http.IgniteRestHttpSessionConfiguration;
+import org.apache.ignite.springsession.model.AuthContext;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
